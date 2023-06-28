fb9cb566e5a465c67fc148a4837964f8e7f86107
AJITH KUMAR M <89124776+ajitharunai@users.noreply.github.com>
2023-06-28 11:46:09 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,13 +20,13 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Aji Creations ",
-  title: "Hi all, I'm Ajith Kumar Murugan",
+  username: "Ajith Kumar Murugan",
+  title: "Hi all, I'm Ajith",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1Wj7YHlysTP9gSxt9NziBrXAQLaDJN7xT/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
